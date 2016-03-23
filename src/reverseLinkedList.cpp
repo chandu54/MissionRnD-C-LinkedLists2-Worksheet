a3c23cd8157955adfe5eec9e9f8ce1b30d91cca3
chandu54 <chandu54@missionrnd.com>
2016-03-23 13:14:00 +0530

    Submit Commit

--- src/reverseLinkedList.cpp
+++ src/reverseLinkedList.cpp
@@ -19,5 +19,21 @@ struct node {
 };
 
 struct node * reverseLinkedList(struct node *head) {
-	return NULL;
+	struct node* temp,*temp1;
+	struct node* end = NULL;
+	temp = head;
+	if (temp == NULL)
+		return NULL;
+	if (temp->next == NULL)
+		return head;
+	end = NULL;
+	while (temp != NULL)
+	{
+		temp1 = end;
+		end = temp;
+		temp = temp->next;
+		end->next = temp1;
+	}
+	head = end;
+	return head;
 }
