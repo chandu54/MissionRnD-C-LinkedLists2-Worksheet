b14ca8c60a3b53b99220385f0e29e0b0f1331d07
chandu54 <chandu54@missionrnd.com>
2016-03-25 13:19:54 +0530

    Submit Commit

--- src/linkedListMedian.cpp
+++ src/linkedListMedian.cpp
@@ -18,7 +18,35 @@ struct node {
 	int num;
 	struct node *next;
 };
-
 int linkedListMedian(struct node *head) {
-	return -1;
+	struct node*temp, *temp1 = head, *temp2 = head, *temp3 = head, *temp4 = head, *temp5;
+	int count = 0,i=1,mid;
+	temp = head;
+	if (temp == NULL)
+		return -1;
+	while (temp2 != NULL)
+	{
+		count++;
+		temp2 = temp2->next;
+	}
+	if (count % 2 == 0)
+	{
+		while (i < count / 2)
+		{
+			temp3 = temp3->next;
+			i++;
+		}
+		mid = ((temp3->num) + (temp3->next->num)) / 2;
+		return mid;
+	}
+	else
+	{
+	while (temp1->next != NULL)
+	{
+		temp = temp->next; 
+		temp1 = temp1->next->next;
+	}
+	mid = temp->num;
+	return mid;
+	}
 }
